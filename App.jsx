ab02e2efdeab5cb3d0af803de30cf9af3b97a886
Muhammad Talha <muhammadtalhaafzal951@gmail.com>
2026-07-12 15:24:32 +0500

    Completed GameFlix with 3 pages

--- App.jsx
+++ App.jsx
@@ -1,21 +1,21 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Home'
-import Trending from './Trending'
-import Playlist from './Playlist'
-import Review from './Review'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Trending from "./Trending";
+import Playlist from "./Playlist";
+import Review from "./Review";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Trending" element={<Trending />}/>
-        <Route path="/Playlist" element={<Playlist />}/>
-        <Route path="/Review" element={<Review />}/>
+        <Route path="/Trending" element={<Trending />} />
+        <Route path="/Playlist" element={<Playlist />} />
+        <Route path="/Review" element={<Review />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
--- Playlist.jsx
+++ Playlist.jsx
@@ -1,79 +1,90 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 const Playlist = () => {
-    const urlData = 'https://www.freetogame.com/api/games';
-    const [genres, setGenres] = useState([]);
-    const [selectedGenre, setSelectedGenre] = useState('');
-    const [isPending, setIsPending] = useState(true);
-    const [allGames, setAllGames] = useState([]);
-    const [relatedGames, setRelatedGames] = useState([]);
-    
-    useEffect(() => {
-        fetch(urlData)
-        .then(res => {
-            if (!res.ok) {
-                 throw Error('There is an Error in response');
-            }
-            return res.json();
-        })
-        .then(data => {
-            const RepeatedGenres = data.map((x) => x.genre)
-            const UniqueGenres = [...new Set(RepeatedGenres)]
-            setAllGames(data);
-            setGenres(UniqueGenres);
-            setSelectedGenre('');
-            setIsPending(false);
-        })
-        .catch(err => {
-            console.error(err);
-            setIsPending(false);
-        })
-    }, [])
+  const urlData = "https://www.freetogame.com/api/games";
+  const [genres, setGenres] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState("");
+  const [allGames, setAllGames] = useState([]);
+  const [relatedGames, setRelatedGames] = useState([]);
 
-    useEffect(() => {
-        if(selectedGenre) {
-            const filteredGames = allGames.filter(game => game.genre === selectedGenre);
-            setRelatedGames(filteredGames);
-        } else {
-            setRelatedGames([]);
+  useEffect(() => {
+    fetch(urlData)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("There is an Error in response");
         }
-    }, [selectedGenre, allGames])
+        return res.json();
+      })
+      .then((data) => {
+        const RepeatedGenres = data.map((x) => x.genre);
+        const UniqueGenres = [...new Set(RepeatedGenres)];
+        setAllGames(data);
+        setGenres(UniqueGenres);
+        setSelectedGenre("");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
-    const handleGenre = (event) => {
-        setSelectedGenre(event.target.value);
+  useEffect(() => {
+    if (selectedGenre) {
+      const filteredGames = allGames.filter(
+        (game) => game.genre === selectedGenre,
+      );
+      setRelatedGames(filteredGames);
+    } else {
+      setRelatedGames([]);
     }
+  }, [selectedGenre, allGames]);
 
-    return (
-        <div className = "Playlist-Container">
-            <div>
-                <h1 className = "Title-Container">Search PlayList</h1>
-            </div>
+  const handleGenre = (event) => {
+    setSelectedGenre(event.target.value);
+  };
 
-            <div className="Playlist-Controls">
-                <label htmlFor="Select-Genre" className="Genre-Label">Select A Genre :</label>
-                <select id="Select-Genre" value={selectedGenre} onChange={handleGenre} className="Genre-Select">
-                    <option value="">-- Select Genre --</option>
-                    {genres.map((genre, index) => (
-                        <option key={index} value={genre}>{genre}</option>
-                    ))}
-                </select>
-            </div>
+  return (
+    <div className="Playlist-Container">
+      <div>
+        <h1 className="Title-Container">Search PlayList</h1>
+      </div>
 
-            {selectedGenre && (
-                <div className="Games-Container">
-                    {relatedGames.length > 0 ? (
-                        relatedGames.map((game) => (
-                            <div key={game.id} className="Game-Details">
-                                <img src={game.thumbnail} alt={game.title} />
-                                <h3>{game.title}</h3>
-                                <p>{game.genre}</p>
-                                <p>{game.platform}</p>
-                            </div>
-                        ))) : ( <p className="Empty-Text">No games found for this genre</p>)}
-                </div>
-            )}
+      <div className="Playlist-Controls">
+        <label htmlFor="Select-Genre" className="Genre-Label">
+          Select A Genre :
+        </label>
+        <select
+          id="Select-Genre"
+          value={selectedGenre}
+          onChange={handleGenre}
+          className="Genre-Select"
+        >
+          <option value="">-- Select Genre --</option>
+          {genres.map((genre, index) => (
+            <option key={index} value={genre}>
+              {genre}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {selectedGenre && (
+        <div className="Games-Container">
+          {relatedGames.length > 0 ? (
+            relatedGames.map((game) => (
+              <div key={game.id} className="Game-Details">
+                <img src={game.thumbnail} alt={game.title} />
+                <h3>{game.title}</h3>
+                <p>{game.genre}</p>
+                <p>{game.platform}</p>
+              </div>
+            ))
+          ) : (
+            <p className="Empty-Text">No games found for this genre</p>
+          )}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 export default Playlist;
