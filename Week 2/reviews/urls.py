5f549e4ac0175421f7f986b77ef976897b25344f
Muhammad Talha <muhammadtalhaafzal951@gmail.com>
2026-07-22 22:01:00 +0500

    Build a CRUD REST API for one resource

---
+++ Week 2/reviews/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ReviewDetailView, ReviewListCreateView
+
+urlpatterns = [
+    path("reviews/", ReviewListCreateView.as_view()),
+    path("reviews/<int:pk>/", ReviewDetailView.as_view()),
+]
