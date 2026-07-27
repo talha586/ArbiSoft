09eea697facbc9c98486e4d74c0d7cad0e8aeaa9
Muhammad Talha <muhammadtalhaafzal951@gmail.com>
2026-07-28 00:25:23 +0500

    chore: stop tracking previous files

--- Week 2/reviews/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ReviewDetailView, ReviewListCreateView
-
-urlpatterns = [
-    path("reviews/", ReviewListCreateView.as_view()),
-    path("reviews/<int:pk>/", ReviewDetailView.as_view()),
-]
